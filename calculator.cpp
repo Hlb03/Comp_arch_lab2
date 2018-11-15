63039a85d8d387d3e93027c30589a91de9ffa6a1
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    executing cherry-pish with adding own lines

--- calculator.cpp
+++ calculator.cpp
@@ -9,3 +9,16 @@ int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+
+// Add custom changes
+// to calculator.cpp file 
+//after executing cherry-pick
+int Calculator::Div (double a, double b)
+{
+	return a / b + 0.5;
+}
